4f84db4f48759e7fe7ee8958933752ddb9cbd2fc
siddharthsharma7 <siddharthsharma7@gmail.com>
2016-03-24 09:17:05 -0400

    getting ready for JSON

--- view/javascript_func.js
+++ view/javascript_func.js
@@ -1,13 +1,37 @@
-function funcSelectItem()
+$(document).ready(function()
 {
-  // document.write(selected_item);
-   window.location="test.html";
-}
+  /*
+  $("tr, .item").hide();
+  $("h2").click(function() {
+  $("tr, .item").toggle();
+  */
+$("tr").click(function(){
+  alert($("#item_name").text());
+  alert($("#item_price").text());
+  alert($("#category").text());
 
-// $("tr").click(function(e) {
-//
-//   document.write("5");
-//     // console.log(e.target.ID);
-//     // document.write(this.nodeValue);  //  document.write("5");
-//
-// });
+  // alert(document.getElementById('item_name').getText);
+  localStorage.category = $("#category").text();
+  localStorage.item_price = $("#item_price").text();
+  localStorage.item_name = $("#item_name").text();
+
+  appendToStorage('category', $("#category").text());
+
+  function appendToStorage(name, data){
+      var old = localStorage.getItem(name);
+      if(old === null) old = "";
+      localStorage.setItem(name, old + data);
+  }
+
+})
+// $("h2").children("item").toggle();
+});
+
+// function funcSelectItem($id)
+// {
+//   alert(document.getElementById($id).innerHTML);
+// }
+// alert(this.gethtml());
+  // document.write(selected_item);
+  //  window.location="test.html";
+  // });
